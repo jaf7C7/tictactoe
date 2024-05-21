cbbb871acb7d153f66af36d5dcab56ef7fda3790
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-05-21 17:21:43 +0100

    Add `marker` property to `Player` basedon `is_human` attr.

--- test_player.py
+++ test_player.py
@@ -10,6 +10,8 @@ class TestPlayer(TestCase):
         self.assertTrue(human.is_human)
         self.assertFalse(computer.is_human)
 
-    def test_is_human_if_not_human(self):
-        player = Player(is_human=False)
-        self.assertFalse(player.is_human)
+    def test_markers(self):
+        human = Player(is_human=True)
+        computer = Player(is_human=False)
+        self.assertEqual('X', human.marker)
+        self.assertEqual('O', computer.marker)
