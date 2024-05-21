199ad86565bbd254fa28e5a794f6eaef8ca6066b
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-05-21 17:02:33 +0100

    Add `winning_marker` method to `Board`

--- board.py
+++ board.py
@@ -9,3 +9,23 @@ class Board:
 
     def place_marker(self, marker, position):
         self.positions[position - 1] = marker
+
+    def winning_marker(self):
+        marker = None
+        for a, b, c in (
+            (0, 1, 2),
+            (3, 4, 5),
+            (6, 7, 8),
+            (0, 3, 6),
+            (1, 4, 7),
+            (2, 5, 8),
+            (0, 4, 8),
+            (2, 4, 6),
+        ):
+            if (
+                self.positions[a] is not None
+                and self.positions[a] == self.positions[b] == self.positions[c]
+            ):
+                marker = self.positions[a]
+                break
+        return marker
--- test_board.py
+++ test_board.py
@@ -13,3 +13,60 @@ class TestBoard(TestCase):
     def test_place_marker(self):
         self.board.place_marker('X', 3)
         self.assertEqual('X', self.board.positions[2])
+
+    def test_winning_marker(self):
+        for positions in (
+            [
+                'X', 'X', 'X',
+                None, None, None,
+                None, None, None,
+            ],
+            [
+                None, None, None,
+                'X', 'X', 'X',
+                None, None, None,
+            ],
+            [
+                None, None, None,
+                None, None, None,
+                'X', 'X', 'X',
+            ],
+            [
+                'X', None, None,
+                'X', None, None,
+                'X', None, None,
+            ],
+            [
+                None, 'X', None,
+                None, 'X', None,
+                None, 'X', None,
+            ],
+            [
+                None, None, 'X',
+                None, None, 'X',
+                None, None, 'X',
+            ],
+            [
+                'X', None, None,
+                None, 'X', None,
+                None, None, 'X',
+            ],
+            [
+                None, None, 'X',
+                None, 'X', None,
+                'X', None, None,
+            ]
+        ):
+            self.board.positions = positions
+            self.assertEqual(
+                'X', self.board.winning_marker(), msg=self.board.positions
+            )
+
+    def test_winning_marker_no_winner(self):
+        self.board.positions = [
+            None, None, None,
+            None, None, None,
+            'X', 'X', 'O',
+        ]
+        self.assertIs(None, self.board.winning_marker())
+
