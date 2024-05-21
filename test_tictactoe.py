565c17780d8e450be5dcbdf6001eae3e4ad3f850
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-05-21 17:46:49 +0100

    Ensure one human and one not human player in `Tictactoe.players`

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -17,3 +17,7 @@ class TestTictactoe(TestCase):
                 for player in self.tictactoe.players
             )
         )
+
+    def test_player_1_is_human_and_player_2_is_not(self):
+        self.assertTrue(self.tictactoe.players[0].is_human)
+        self.assertFalse(self.tictactoe.players[1].is_human)
--- tictactoe.py
+++ tictactoe.py
@@ -4,4 +4,4 @@ from player import Player
 class Tictactoe:
 
     def __init__(self):
-        self.players = [Player(is_human=True), Player(is_human=True)]
+        self.players = [Player(is_human=True), Player(is_human=False)]
