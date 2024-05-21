c2dfc4e37172a60c2cca9734c8709ffec1a08ec7
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-05-21 17:29:05 +0100

    Add `select_position` method to `Player`

--- player.py
+++ player.py
@@ -1,5 +1,14 @@
+from random import randint
+
+
 class Player:
 
     def __init__(self, is_human):
         self.is_human = is_human
         self.marker = 'X' if self.is_human else 'O'
+
+    def select_position(self):
+        if self.is_human:
+            return input()
+        else:
+            return randint(1, 9)
