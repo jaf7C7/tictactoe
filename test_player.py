c2750232de6ddb44dc6724db448ee1d3172afcbf
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-05-21 17:19:52 +0100

    Refactor test

--- test_player.py
+++ test_player.py
@@ -4,9 +4,11 @@ from player import Player
 
 class TestPlayer(TestCase):
 
-    def test_is_human_if_human(self):
-        player = Player(is_human=True)
-        self.assertTrue(player.is_human)
+    def test_is_human(self):
+        human = Player(is_human=True)
+        computer = Player(is_human=False)
+        self.assertTrue(human.is_human)
+        self.assertFalse(computer.is_human)
 
     def test_is_human_if_not_human(self):
         player = Player(is_human=False)
